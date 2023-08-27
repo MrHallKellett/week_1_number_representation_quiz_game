0e65d7320acb93c9f322be968c409ba5b1ab2685
MrHallKellett <142761867+MrHallKellett@users.noreply.github.com>
2023-08-27 20:24:57 +0800

    Update test_11_convert_number_base.py

--- tests/test_11_convert_number_base.py
+++ tests/test_11_convert_number_base.py
@@ -1,7 +1,7 @@
 import main
 
-assert convert_number_base("2201", 3, 10) = int("2201", 3)
-assert convert_number_base("707532", 8, 2) = bin(int("707532", 8))[2:]
-assert convert_number_base("404044404", 5, 16) = hex(int("404044404", 5))[2:].upper()
-assert convert_number_base("GJI1031AAA", 30, 10) = int("GJI1031AAA", 30)
+assert convert_number_base("2201", 3, 10) == int("2201", 3)
+assert convert_number_base("707532", 8, 2) == bin(int("707532", 8))[2:]
+assert convert_number_base("404044404", 5, 16) == hex(int("404044404", 5))[2:].upper()
+assert convert_number_base("GJI1031AAA", 30, 10) == int("GJI1031AAA", 30)
 
