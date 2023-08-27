a76c9eed5b9a13894a497be18c31fd96c4204e3f
MrHallKellett <142761867+MrHallKellett@users.noreply.github.com>
2023-08-27 20:24:38 +0800

    Update test_09_convert_dec_to_bin_2s_mode.py

--- tests/test_09_convert_dec_to_bin_2s_mode.py
+++ tests/test_09_convert_dec_to_bin_2s_mode.py
@@ -1,8 +1,8 @@
 
 import main
 
-assert convert_dec_to_bin_2s_mode(5, True) = "0101"
-assert convert_dec_to_bin_2s_mode(-55, True) = "1001001"
-assert convert_dec_to_bin_2s_mode(-5555, True) = "10101001001101" # check this later
-assert convert_dec_to_bin_2s_mode(555, True) = "0" + bin(555)[2:]
-assert convert_dec_to_bin_2s_mode(-12, True) = "10100" 
+assert convert_dec_to_bin_2s_mode(5, True) == "0101"
+assert convert_dec_to_bin_2s_mode(-55, True) == "1001001"
+assert convert_dec_to_bin_2s_mode(-5555, True) == "10101001001101" # check this later
+assert convert_dec_to_bin_2s_mode(555, True) == "0" + bin(555)[2:]
+assert convert_dec_to_bin_2s_mode(-12, True) == "10100" 
