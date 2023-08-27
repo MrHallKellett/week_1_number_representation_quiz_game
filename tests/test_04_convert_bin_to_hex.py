d6cb93e796091f31c5399810ab494d5383ffc371
MrHallKellett <142761867+MrHallKellett@users.noreply.github.com>
2023-08-27 20:23:35 +0800

    Update test_04_convert_bin_to_hex.py

--- tests/test_04_convert_bin_to_hex.py
+++ tests/test_04_convert_bin_to_hex.py
@@ -1,7 +1,7 @@
 import main
 
-assert convert_bin_to_hex("1") = "1""
-assert convert_bin_to_hex("1011") = "B"
-assert convert_bin_to_hex("100001000") = "108"
-assert convert_bin_to_hex("10101110111") = hex(int("10101110111", 2))[2:].upper()
-assert convert_bin_to_hex("01010101110000001111000") = hex(int("01010101110000001111000", 2))[2:].upper()
+assert convert_bin_to_hex("1") == "1""
+assert convert_bin_to_hex("1011") == "B"
+assert convert_bin_to_hex("100001000") == "108"
+assert convert_bin_to_hex("10101110111") == hex(int("10101110111", 2))[2:].upper()
+assert convert_bin_to_hex("01010101110000001111000") == hex(int("01010101110000001111000", 2))[2:].upper()
