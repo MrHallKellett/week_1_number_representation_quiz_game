a4c07bdd609593357663941ad2cddc1a6baffb01
MrHallKellett <142761867+MrHallKellett@users.noreply.github.com>
2023-08-27 20:23:44 +0800

    Update test_05_convert_hex_to_bin.py

--- tests/test_05_convert_hex_to_bin.py
+++ tests/test_05_convert_hex_to_bin.py
@@ -1,7 +1,7 @@
 import main
 
-assert convert_hex_to_bin("F01") = "111100000001"
-assert convert_hex_to_bin("45444") = "01000101010001000100"
-assert convert_hex_to_bin("900000A") = "10010000000000000000000001010"
-assert convert_hex_to_bin("54321") = "01010100001100100001"
-assert convert_hex_to_bin("ABCDEF") = "101010111100110111101111"
+assert convert_hex_to_bin("F01") == "111100000001"
+assert convert_hex_to_bin("45444") == "01000101010001000100"
+assert convert_hex_to_bin("900000A") == "10010000000000000000000001010"
+assert convert_hex_to_bin("54321") == "01010100001100100001"
+assert convert_hex_to_bin("ABCDEF") == "101010111100110111101111"
