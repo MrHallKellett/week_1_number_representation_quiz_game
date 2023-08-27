f07664d05b3bce1bb448a4e917f9506980c2f5c5
MrHallKellett <142761867+MrHallKellett@users.noreply.github.com>
2023-08-27 20:05:55 +0800

    Create test_08_test_binary_subtract.py

---
+++ tests/test_08_test_binary_subtract.py
@@ -0,0 +1,7 @@
+import main
+
+  
+assert binary_subtract("111", "111") == "000"
+assert binary_subtract("01110", "00001") == "01101"
+assert binary_subtract("0011", "0101") == "1110"
+assert binary_subtract("01011110", "00011111") == "10111111"
