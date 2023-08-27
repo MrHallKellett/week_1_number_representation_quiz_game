d02c93627ab94d2f0fdc792fb0784eb645e5b131
MrHallKellett <142761867+MrHallKellett@users.noreply.github.com>
2023-08-27 13:32:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from tests import run_tests
+## SECTION A
 
+## your code goes here
+
+
+
+#########################################
+
+## SECTION B
+
+def game():
+  """Your number representation quiz game code here"""
+
+
+
+
+#########################################
 
 if __name__ == "__main__":
 
-  run_tests()
+  pass
