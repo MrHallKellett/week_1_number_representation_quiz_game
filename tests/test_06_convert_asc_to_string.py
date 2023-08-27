b6fe0c313ef918cb53d2fcca081dc26aa3f038c7
MrHallKellett <142761867+MrHallKellett@users.noreply.github.com>
2023-08-27 16:11:23 +0800

    Update and rename test_06_convert_asc_to_string.py to test_06_test_bin_add.py

--- tests/test_06_convert_asc_to_string.py
+++
@@ -1,8 +0,0 @@
-import main
-
-assert convert_asc_to_string('a') = "1100001"
-assert convert_asc_to_string('cat') = "110001111000011110100"
-assert convert_asc_to_string('S@t') = "101001110000001110100"
-assert convert_asc_to_string('ONNNN') = "10011111001110100111010011101001110"
-assert convert_asc_to_string('a mat !?') = "11000010100000110110111000011110100010000001000010111111"
-
---
+++ tests/test_06_test_bin_add.py
@@ -0,0 +1,6 @@
+import main
+
+assert add_binary('1000000', '100001') = "1100001"
+assert add_binary('111', '111') = "1110"
+assert add_binary('101010', '111') = "110001"
+
